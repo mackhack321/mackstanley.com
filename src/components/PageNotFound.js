8c2b12b1e91699ca5a398903985f1755bd2b1ed5
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 17:59:38 -0600

    improve 404 page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
   return (
-    <div className="grid place-items-center">
-      <div className="py-5 font-code text-3xl font-bold text-mack-white md:text-4xl">
-        404 Page Not Found
+    <div>
+      <div className="py-5 text-center font-code text-3xl font-bold text-mack-white md:text-4xl">
+        404
+        <br />
+        Page Not Found
       </div>
-      <Link to="/" className="text-mack-white underline">
+      <Link to="/" className="flex justify-center text-mack-white underline">
         Go home
       </Link>
     </div>
