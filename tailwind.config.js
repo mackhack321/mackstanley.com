b6e96795272c921b117c15adf7857c55c1f93be1
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 17:38:53 -0600

    rename code font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      header: "Fira Code",
+      code: "Fira Code",
     },
     extend: {
       colors: {
