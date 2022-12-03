73aa4416ddc2aef114f64f4b55780eeccf5173e4
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 17:47:54 -0600

    make 404 page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
-  return <div className="font-code text-mack-white">PageNotFound</div>;
+  return (
+    <div className="grid place-items-center">
+      <div className="font-code text-mack-white text-3xl md:text-4xl font-bold py-5">
+        404 Page Not Found
+      </div>
+      <Link to="/" className="text-mack-white underline">
+        Go home
+      </Link>
+    </div>
+  );
 }
