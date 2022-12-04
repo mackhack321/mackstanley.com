7d07919ec12650342812f0cccdfe788c361a72c0
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 20:09:23 -0600

    give 404 page heading structure

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function PageNotFound() {
   return (
     <div>
-      <div className="pb-5 text-center font-code text-3xl font-bold text-mack-white md:text-4xl">
+      <h1 className="pb-5 text-center font-code text-3xl font-bold text-mack-white md:text-4xl">
         404
         <br />
         Page Not Found
-      </div>
+      </h1>
       <Link to="/" className="flex justify-center text-mack-white underline">
         Go home
       </Link>
