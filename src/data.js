cd6c2d27f2989dba23216e6d225c7d62c1681c48
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 22:35:11 -0600

    extract data to its own file

---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const pages = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Experience",
+    link: "experience",
+  },
+  {
+    title: "Projects",
+    link: "projects",
+  },
+];
+
+export const favorites = [
+  {
+    category: "Food",
+    items: ["Sushi", "Cheeseburgers", "Catfish", "Spaghetti", "Pad Thai"],
+  },
+  {
+    category: "Music",
+    items: ["Folk Rock", "Rap", "Rock", "Classical", "Pop"],
+  },
+  {
+    category: "Season",
+    items: ["Winter"],
+  },
+  {
+    category: "Color",
+    items: ["Dark purple"],
+  },
+  {
+    category: "Video Game",
+    items: ["Destiny 2"],
+  },
+];
+
+export const skills = [
+  "React",
+  "Tailwind CSS",
+  "Python",
+  "Linux",
+  "C++",
+  "Googling",
+  "Cooking spaghetti",
+  "Git",
+  "Restarting the router when the internet goes out",
+  "Documentation",
+  "Team leadership",
+  "Agile software development",
+  "Scrum master",
+];
