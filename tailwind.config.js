a6a6e3368f5856d4fea82caf6ad10533fbf66c38
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 22:35:17 -0600

    add mack blue for links

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "mack-purple": "#581C87",
         "mack-slate": "#262626",
         "mack-white": "#F1F5F9",
+        "mack-blue": "#93c5fd",
       },
       screens: {
         "3xl": "2750px",
