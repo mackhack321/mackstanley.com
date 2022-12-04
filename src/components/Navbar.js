412216b1e4a81bc0b8de9726e96d87cbffce9ada
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 21:03:50 -0600

    little navbar improvements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../assets/mackpc.jpg";
+import logo from "../assets/mackpc.png";
 import { Link } from "react-router-dom";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
@@ -24,17 +24,16 @@ export default function Navbar() {
 
   return (
     <header>
-      <nav className="grid min-h-[75px] place-items-center bg-mack-purple">
-        <div className="w-full self-center px-10 text-mack-white lg:w-2/3 lg:px-0 3xl:w-1/3">
+      <nav className="mb-5 grid min-h-[75px] place-items-center bg-mack-purple">
+        <div className="w-full px-10 text-mack-white lg:w-2/3 lg:px-0 3xl:w-1/3">
           <div className="flex items-center">
             <Link to="/" className="mr-10">
               <img
                 src={logo}
                 alt="Mack's logo, a pixel-art style drawing of an old PC"
-                className="w-[50px]"
               />
             </Link>
-            <div className="hidden space-x-5 md:flex">
+            <div className="hidden space-x-10 md:flex">
               {pages.map(({ link, title }) => {
                 return (
                   <Link to={link} className="font-code" key={title}>
@@ -53,14 +52,14 @@ export default function Navbar() {
               <div className="font-code">Download my résumé (PDF)</div>
             </a>
             <Bars3Icon
-              className="ml-auto h-[44px] stroke-2 md:hidden"
+              className="ml-auto h-[44px] cursor-pointer stroke-2 md:hidden"
               onClick={() => setShowMenu(!showMenu)}
             />
           </div>
           {/* mobile nav menu */}
           <div
-            className={`mt-5 flex flex-col space-y-5 pb-5 md:hidden ${
-              !showMenu ? "hidden" : ""
+            className={`mt-5 flex flex-col space-y-5 pb-5 ${
+              !showMenu ? "hidden" : "md:hidden"
             }`}
           >
             {pages.map(({ link, title }) => {
