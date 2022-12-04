dc425a3c44e65ea3470ae8f06c84ae38a03c0b1d
Mack Stanley <mlgprohacker@gmail.com>
2022-12-03 20:08:45 -0600

    navbar has padding so headers only need pb not py

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function PageNotFound() {
   return (
     <div>
-      <div className="py-5 text-center font-code text-3xl font-bold text-mack-white md:text-4xl">
+      <div className="pb-5 text-center font-code text-3xl font-bold text-mack-white md:text-4xl">
         404
         <br />
         Page Not Found
