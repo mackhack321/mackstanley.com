a2a40a45823be39ebc8a60eb744a389a1e3d4496
Mack Stanley <mlgprohacker@gmail.com>
2022-12-04 21:20:51 -0600

    justify-center experiences

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,7 +12,7 @@ export default function Experience(props) {
   } = props;
   return (
     <div>
-      <div className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-5">
+      <div className="flex flex-col space-y-5 md:flex-row md:justify-center md:space-y-0 md:space-x-5">
         <div>
           <img
             src={logo}
