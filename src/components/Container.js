cd40829a2fd878a6c2b150ec3f343ab21f55d89b
Mack Stanley <mlgprohacker@gmail.com>
2022-12-09 16:20:14 -0600

    even better breakpoints

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,7 +7,7 @@ export default function Container({ children }) {
     <div>
       <Navbar />
       <main className="grid place-items-center">
-        <div className="w-full px-10 xl:w-2/3 xl:px-0 3xl:w-1/3">
+        <div className="w-full px-10 2xl:w-2/3 2xl:px-0 3xl:w-1/3">
           {children}
         </div>
       </main>
